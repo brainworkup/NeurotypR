51135deeb3eccafc8af5b9e960cb3447dfe61c8c
brainworkup <j.trampush@gmail.com>
2025-01-17 09:55:02 -0800

    not sure

--- inst/rmarkdown/templates/neurotypr/skeleton/Rmd/params.R
+++ inst/rmarkdown/templates/neurotypr/skeleton/Rmd/params.R
@@ -1,18 +1,32 @@
 # Parameters
 
-patient <- "Biggie"
+patient <- "Eric"
 
-# test <- "wais5"
-# test_name <- "WAIS-5"
-# pages <- c(5, 5)
-# extract_columns <- c(1, 3, 4, 5)
+# WAIS5 subtests
+test <- "wais5"
+test_name <- "WAIS-V"
+pages <- c(5)
+extract_columns <- c(2, 4, 5, 6)
+variables <- c("scale", "raw_score", "score", "percentile")
+score_type <- "scaled_score"
 
-test <- "wrat5"
-test_name <- "WRAT-5"
-pages <- c(8)
+# WAIS5 composites
+test <- "wais5"
+test_name <- "WAIS-V"
+pages <- c(7)
 extract_columns <- c(1, 2, 3, 4, 5)
+variables <- c("scale", "abbrev", "raw_score", "score", "percentile", "ci_95")
 score_type <- "standard_score"
 
+# WRAT5
+test <- "wrat5"
+test_name <- "WRAT-5"
+pages <- c(2)
+extract_columns <- c(1, 2, 3, 4, 5)
+score_type <- "standard_score"
+variables <- c("scale", "raw_score", "score", "ci_95", "percentile")
+
+# WMS4
 test <- "wms4"
 test_name <- "WMS-IV"
 pages <- c(12)
@@ -20,8 +34,11 @@ extract_columns <- c(1, 3, 4, 5)
 variables <- c("scale", "raw_score", "score", "percentile")
 score_type <- "scaled_score"
 
+# File and path
 file <- file.path(file.choose())
+qs::qsave(file, paste0(test, "_path.rds"))
 
+# Parameters
 params <-
   list(
     patient = patient,
@@ -30,7 +47,8 @@ params <-
     file = file,
     pages = pages,
     extract_columns = extract_columns,
-    score_type = score_type
+    score_type = score_type,
+    variables = variables
   )
 
 # Extracted areas
@@ -48,7 +66,7 @@ for (i in seq_along(extracted_areas)) {
 }
 
 # Save the entire list to an R data file
-save(extracted_areas, file = paste0(test, "_extracted_areas.RData"))
+save(extracted_areas, file = paste0(test, "_extracted_areas.rds"))
 
 # Check the extracted areas
 str(extracted_areas)
