546f29f78768ee111f24c809814c4b45ae4641a1
brainworkup <j.trampush@gmail.com>
2025-02-11 09:27:10 -0800

    REDO of repo trying to fix things

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -5,18 +5,12 @@
 #     ln -s -- "$f" /Users/joey/neurotyp/NeurotypR/Rmd/"$(basename "$f")"
 #   done' find-sh {} \;
 
-# find /Users/joey/neurotyp/NeurotypR/inst/rmarkdown/templates/neurotypr/skeleton -name '*.Rmd' -exec sh -c '
-#   for f do
-#     ln -s -- "$f" /Users/joey/reports/Biggie/Rmd/"$(basename "$f")"
-#   done' find-sh {} \;
-
-
-# find /Users/joey/neurotyp/NeurotypR/inst/rmarkdown/templates/neurotypr/skeleton/quarto -name '*.qmd' -exec sh -c '
-#   for f do
-#     ln -s -- "$f" /Users/joey/neurotyp/NeurotypR/quarto/"$(basename "$f")"
-#   done' find-sh {} \;
-
-find /Users/joey/neurotyp/NeurotypR/inst/rmarkdown/templates/neurotypr/skeleton -name '*.R' -exec sh -c '
+find /Users/joey/neurotyp/NeurotypR/inst/rmarkdown/templates/neurotypr/skeleton/quarto -name '*.qmd' -exec sh -c '
   for f do
-    ln -s -- "$f" /Users/joey/neurotyp/NeurotypR/Rmd/"$(basename "$f")"
+    ln -s -- "$f" /Users/joey/neurotyp/NeurotypR/Qmd/"$(basename "$f")"
   done' find-sh {} \;
+
+# find /Users/joey/neurotyp/NeurotypR/inst/rmarkdown/templates/neurotypr/skeleton -name '*.R' -exec sh -c '
+#   for f do
+#     ln -s -- "$f" /Users/joey/neurotyp/NeurotypR/Rmd/"$(basename "$f")"
+#   done' find-sh {} \;
